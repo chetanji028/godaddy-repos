97bff94451a5c3319eec7b8d566831cdec8ba353
chetanji028 <chetanji028@gmail.com>
2025-07-10 14:01:46 +0530

    Initial commit of GoDaddy Repositories React app

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+ 
+import RepoList from '../components/RepoList';
+
+const Home = () => (
+  <div className="container mx-auto p-4">
+    <h1 className="text-3xl font-bold text-center mb-6">GoDaddy GitHub Repositories</h1>
+    <RepoList />
+  </div>
+);
+
+export default Home;
